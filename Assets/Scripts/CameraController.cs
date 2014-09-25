d10a32a0995346c2dfa1a8e0e77170129a68c946
adisib <alexdisibio@gmail.com>
2014-09-24 22:19:37 -0400

    ControlImprovement
    Better button input responsiveness, feedback with things such as screen
    wobble during sprint, attack range improvement, and speed fix.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,8 +8,7 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	// TODO
-	// fix FOV is not clamping for some reason
-	// add camera wobble (to be used in sprinting) if we want to add it
+	// figure out why localeulerangles isn't setting
 
 	public Transform player;
 	public float cameraXSpeed;
@@ -25,13 +24,15 @@ public class CameraController : MonoBehaviour {
 	private float minFOV = 30.0f;
 	private float maxFOV = 90.0f;
 	private float currentPitch = 0.0f;
-	// private int wobbleDir = -1;
-	// private float wobbleModifier = 0.1f;
+	private Transform cameraTransform;
+	private Vector3 newAngles;
 	
 	void Start ()
 	{
-		transform.position = player.position + offset; 
-		transform.LookAt(player.position);
+		cameraTransform = GetComponent<Transform>();
+
+		cameraTransform.position = player.position + offset; 
+		cameraTransform.LookAt(player.position);
 	}
 
 	void LateUpdate ()
@@ -54,11 +55,7 @@ public class CameraController : MonoBehaviour {
 		player.transform.Rotate(Vector3.up * xInput);
 
 		currentPitch = Mathf.Clamp(currentPitch + yInput, minY, maxY);
-		transform.localEulerAngles = Vector3.right * currentPitch;
-	}
-
-	public static IEnumerator cameraWobble() // TODO
-	{
-		yield return null;
+		newAngles.Set(currentPitch, cameraTransform.localEulerAngles.y, cameraTransform.localEulerAngles.z);
+		cameraTransform.localEulerAngles = newAngles;
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,9 +9,11 @@ public class PlayerController : MonoBehaviour
 {
 	// TODO
 	// add animation plays once animations are available
+	// add UI response to controls
 	// add item and ability and item usage once available
 	// decide input controls
 	// make attribute dependant stuff dependant on attributes once added
+	// learn spherecast behavior and adjust it
 
 	// Ideas for control scheme
 	// left click attacks, right click uses ability or item, whichever is selected
@@ -26,101 +28,141 @@ public class PlayerController : MonoBehaviour
 	public float jumpModifier;
 	public float sprintModifier;
 	public float jumpDelay; // can change to when contacting ground, but I think there should be longer delay for a bear
+	public Transform cameraTransform;
 
 	public float attackRange; // get this from attribute once attribute class is available
-	public float hitForce; // Your strength, get this from attribute once attribute class is available
+	public float hitForce; // Your strength (*100?), get this from attribute once attribute class is available
 	public float stamina; // sprint duraction, get this from attribute once attribute class is available
 
 	private Vector3 m_playerMovement;
 	private float m_moveX;
 	private float m_moveY;
+	private float m_XModifier;
+	private float m_YModifier;
 	private float m_jumpTime;
 	private int m_selectedAbility;
 	private int m_selectedItem;
-	private bool isSprinting;
+	private bool m_isAbility;
+	private int m_numAbilities = 0; // number of different abilities for selection
+	private int m_numItems = 0; // number of different abilities for selection
+	private float m_weightModifier;
+	private bool m_isSprinting;
 
 	void Start()
 	{
 		m_jumpTime = 0;
 		m_selectedItem = 0;
 		m_selectedAbility = 0;
-		isSprinting = false;
+		m_isSprinting = false;
+		m_weightModifier = GetComponent<Rigidbody>().mass;
 	}
 	
 	void FixedUpdate () // decide controls to use and edit this when agreed upon
 	{
 		movePlayer();
+	}
 
-		/*
-		if(Input.GetButton("Sprint"))
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.LeftShift) && (stamina > 0)) // Sprint, make customizable later
 		{
-			isSprinting = true;
-			// StartCoroutine("cameraWobble");
-			stamina -= Time.deltaTime; // or a factor of the time, depending how stamina is implemented
+			m_isSprinting = true;
+			StartCoroutine(cameraWobble());
+			stamina -= 10*Time.deltaTime;
+			Mathf.Clamp(stamina, 0, Mathf.Infinity);
 		}
-
-		if(Input.GetButtonUp("Sprint"))
+		
+		if(Input.GetKeyUp(KeyCode.LeftShift) || (stamina <= 0)) // Sprint, make customizable later
 		{
-			isSprinting = false;
-			// StopCoroutine("cameraWobble");
+			m_isSprinting = false;
+			// coroutine for delayed stamina gain
 		}
-		*/
-
-		if(Input.GetButtonDown("Jump"))
+		
+		if(Input.GetButtonDown("Jump")) // Jump
 		{
 			jumpPlayer();
 		}
-
-		if(Input.GetButtonDown("Fire1"))
+		
+		if(Input.GetButtonDown("Fire1")) // Attack
 		{
 			playerAttack();
 		}
-
-		if(Input.GetButtonDown("Fire2"))
+		
+		if(Input.GetButtonDown("Fire2")) // Use item or ability
 		{
-			useAbility(m_selectedAbility);
+			if(m_isAbility)
+			{
+				useAbility(m_selectedAbility);
+			}
+			else
+			{
+				useItem(m_selectedItem);
+			}
 		}
-
-		if(Input.GetButtonDown("Fire3"))
+		
+		if(Input.GetButtonDown("SelectAbility")) // select ability, default 'E', make customizable later
 		{
-			useItem(m_selectedItem);
+			if(!m_isAbility)
+			{
+				m_isAbility = true;
+				// Add to UI that ability is selected
+			}
+			else
+			{
+				m_selectedAbility++;
+				if(m_selectedAbility > m_numAbilities)
+				{
+					m_selectedAbility = 1;
+				}
+			}
 		}
-
-		/*
-		if(Input.GetButtonDown())
+		
+		if(Input.GetButtonDown("SelectItem")) // select item, default 'Q', make customizable later
 		{
-			// ability selector todo - just cycle possible int values
+			if(m_isAbility)
+			{
+				m_isAbility = false;
+				// Add to UI that item is selected
+			}
+			else
+			{
+				m_selectedItem++;
+				if(m_selectedItem > m_numItems)
+				{
+					m_selectedItem = 1;
+				}
+			}
 		}
-
-		if(Input.GetButtonDown())
-		{
-			// item selector todo - just cycle possible int values
-		}
-		*/
 
 		increaseTimes(); // for stuff like jumpTime;
 	}
 
 	void movePlayer()
 	{
+		m_XModifier = 1;
+		m_YModifier = 1;
 		m_moveX = Input.GetAxis("Horizontal");
 		m_moveY = Input.GetAxis("Vertical");
 
-		m_moveX *= sideSpeed;
-		m_moveY *= forwardSpeed;
+		if(m_isSprinting)
+		{
+			m_YModifier *= sprintModifier;
+		}
+
+		m_XModifier *= sideSpeed / m_weightModifier;
+		m_YModifier *= forwardSpeed / m_weightModifier;
 
 		if(m_moveY < 0)
 		{
-			m_moveY /= backwardsModifier;
+			m_YModifier /= backwardsModifier;
 		}
-		if(isSprinting)
-		{
-			m_moveX *= sprintModifier;
-		}
-		
+
+		m_moveX *= m_XModifier;
+		m_moveY *= m_YModifier;
+
 		m_playerMovement.Set(m_moveX, 0.0f, m_moveY);
 		
-		transform.Translate(Vector3.ClampMagnitude(m_playerMovement, forwardSpeed));
+		transform.Translate(Vector3.ClampMagnitude(m_playerMovement, Mathf.Abs(m_YModifier)));
 	}
 
 	void jumpPlayer()
@@ -167,13 +209,14 @@ public class PlayerController : MonoBehaviour
 		RaycastHit objectHit;
 		// Swipe animation or however visual is wanted
 		if(Physics.SphereCast(transform.position,
-		                              attackRange,
+		                              attackRange/2,
 		                              transform.TransformDirection(Vector3.forward),
 		                              out objectHit,
 		                              attackRange))
 		{
 			objectHit.rigidbody.AddForce(objectHit.normal*hitForce*-1);
-			if(objectHit.transform.gameObject.tag == "Destructible")
+			if(objectHit.transform.gameObject.tag == "Destructible" || // in-animate things that can be destroyed
+			   objectHit.transform.gameObject.tag == "Target") // or whatever the tag for people is
 			{
 				// play destroyed animation
 				StartCoroutine(GameController.animDestroy(objectHit.transform.gameObject, 1.0f)); // delayed destroy
@@ -184,5 +227,21 @@ public class PlayerController : MonoBehaviour
 	void increaseTimes()
 	{
 		m_jumpTime -= Time.deltaTime;
+	}
+
+	IEnumerator cameraWobble()
+	{
+		float wobbleModifier = 0.3f;
+		
+		cameraTransform.Rotate(Vector3.forward * -wobbleModifier/2);
+		yield return new WaitForSeconds(0.1f);
+		while(m_isSprinting)
+		{
+			cameraTransform.Rotate(Vector3.forward * wobbleModifier);
+			yield return new WaitForSeconds(0.1f);
+			cameraTransform.Rotate(Vector3.forward * -wobbleModifier);
+			yield return new WaitForSeconds(0.1f);
+		}
+		cameraTransform.Rotate(Vector3.forward * wobbleModifier/2);
 	}
 }
