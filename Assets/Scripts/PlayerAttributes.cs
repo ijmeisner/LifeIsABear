05b14bb9d069d6726e8c380d74049e1a06b4e4f0
gctiger <ceverett@georgetowncollege.edu>
2014-09-29 17:25:30 -0400

    PlayerInventory and PlayerAttributes
    Methods added to PlayerAttributes.cs to allow for full health heals and
    energy regeneration following leveling up. PlayerInventory GUI is now
    being focused on.

--- Assets/Scripts/PlayerAttributes.cs
+++ Assets/Scripts/PlayerAttributes.cs
@@ -3,39 +3,86 @@ using System.Collections;
 
 public class PlayerAttributes : MonoBehaviour {
 	
-	public float health = 10.0f;		// Player max health.
-	public float curHealth;				// Current health of player.
-	public float moveSpeed;				// Base movement speed of character
-	public float strength = 2.0f;		// Base damage dealt by abilities.
-	public float attRange = 2.5f;		// Max distance abilities can be used from the player.
+	public float health;		// Player max health.
+	public float curHealth;		// Current health of player.
+	public float moveSpeed;		// Base movement speed of character
+	public float strength;		// Base damage dealt by abilities.
+	public float attRange;		// Max distance abilities can be used from the player.
 	
-	public float endurance = 5.0f;		// Base resource pool for sprinting, abilities, etc.
-	public float curEndurance;			// Current endurance level.
-	public float regeneration = 0.2f;	// Energy regeneration rate.
-	public int[] inventory = new int[5];// Max number of items player can have in inventory.
-	public int curLevel = 1;			// Level of player.
-	public int maxLevel;				// Maximum player level.
-	public int curExp = 1;				// Base amount of experience on current level.
-	private int maxExp = 100;			// Experience needed to level
-	public bool hungry;					//
+	public float endurance;		// Base resource pool for sprinting, abilities, etc.
+	public float curEndurance;	// Current endurance level.
+	public float regeneration;	// Energy regeneration rate.
+
+	public int curLevel;		// Level of player.
+	public int maxLevel;		// Maximum player level.
+	public int curExp;			// Base amount of experience on current level.
+	public int maxExp;			// Experience needed to level
+	public enum hungerLevel{happy, starving, chubby};	// Levels of hunger 
 	
 	
 	void Start() {
+		health = 10.0f;
 		curHealth = health;
+		endurance = 5.0f;
+
 		curEndurance = endurance;
+		regeneration = 1.5f;
+
+		strength = 2.0f;
+		attRange = 2.5f;
+
+		curLevel = 1;
+		maxLevel = 3;
+		curExp = 1;
+		maxExp = 10;
 	}
 	
 	void Update() {
-		// Increment level after curExp reaches maxExp in value
-		// Reset curExp to 1
+
+		LevelUp ();
+		EnergyRegen();
+		Health ();
+
+		// Test for upper or lower boundaries.
+		if (Input.GetKeyDown (KeyCode.H)) {
+			curHealth -= 1;
+		}
+		// Test for upper or lower boundaries.
+		if (Input.GetKeyDown (KeyCode.E)) {
+			curEndurance -= 1;
+		}
+		// Test for upper or lower boundaries.
+		if (Input.GetKeyDown (KeyCode.L)) {
+			curExp += 1;
+		}
+	}
+
+	//-----------------------------------------------------
+	// Level Up
+	// Increment level after curExp reaches maxExp in value
+	// Reset curExp to 1
+	// Increase base stats on level up.
+	//-----------------------------------------------------
+	void LevelUp(){
 		if (curExp >= maxExp) {
 			curExp = 1;
-			curLevel++;
+			if(curLevel < maxLevel){
+				curLevel++;
+				strength += curLevel;
+				health += 5;
+				curHealth = health;
+				endurance += 2;
+				curEndurance = endurance;
+			}
 		}
-		
-		// Energy regeneration
-		// curEndurance values are always greater than or equal
-		// to 0 or less than or equal to endurance.
+	}
+
+	//-----------------------------------------------------
+	// Energy regeneration
+	// curEndurance values are always greater than or equal
+	// to 0 or less than or equal to endurance.
+	//-----------------------------------------------------
+	void EnergyRegen(){
 		if (curEndurance < endurance) {
 			curEndurance += regeneration * Time.deltaTime;
 			if (curEndurance > endurance) {
@@ -45,35 +92,33 @@ public class PlayerAttributes : MonoBehaviour {
 				curEndurance = 0;
 			}	
 		}
-		
-		// Boundaries for health values
+	}
+
+	//-----------------------------------------------------
+	// Health
+	// Checks if curHealth is within 0 and the max
+	// health limit.
+	//-----------------------------------------------------
+	void Health(){
 		if (curHealth > health) {
 			curHealth = health;
 		}
 		if (curHealth < 0) {
 			curHealth = 0;
-		}	
-		
-		// Test for upper or lower boundaries.
-		if (Input.GetKeyDown (KeyCode.E)) {
-			curHealth -= 1;
-		}
-		
-		// Test for upper or lower boundaries.
-		if (Input.GetKeyDown (KeyCode.F)) {
-			curEndurance -= 1;
-		}
-		
-		// Test for upper or lower boundaries.
-		if (Input.GetKeyDown (KeyCode.G)) {
-			curExp += 1;
 		}
 	}
-	
+
+
+	//-----------------------------------------------------
+	// Displays player statistics on ccreen.
+	//-----------------------------------------------------
 	void OnGUI()
 	{
-		GUI.Box(new Rect(20, 50, 200, 20), curHealth + " / " + health);
-		GUI.Box(new Rect(20, 90, 200, 20), "Level: " + curHealth);
+		GUI.Box(new Rect(20, 30, 200, 20), "Level: " + curLevel);
+		GUI.Box(new Rect(20, 50, 200, 20), "Exp: " + curExp + " / " + maxExp);
+		GUI.Box(new Rect(20, 70, 200, 20), "Health: " + curHealth + " / " + health);
+		GUI.Box(new Rect(20, 90, 200, 20), "Stam: " + curEndurance + " / " + endurance);
+		GUI.Box(new Rect(20, 110, 200, 20), "Strength: " + strength);
 		
 	}
 	
---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInventory : MonoBehaviour {
+	public Rect invBackground;
+
+	// Use this for initialization
+	void Start () {
+		invBackground = new Rect(10, 250, 105, 200);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		invBackground = GUI.Window (0, invBackground, showInvSlots, "Inventory");
+	}
+
+	void showInvSlots (int windowId){
+		GUILayout.BeginArea (new Rect(5, 25, 95, 160));
+
+		GUILayout.BeginHorizontal ();
+		GUILayout.Button ("Item 1", GUILayout.Height (50));
+		GUILayout.EndHorizontal ();
+
+		GUILayout.BeginHorizontal ();
+		GUILayout.Button ("Item 2", GUILayout.Height (50));
+		GUILayout.EndHorizontal ();
+
+		GUILayout.BeginHorizontal ();
+		GUILayout.Button ("Item 3", GUILayout.Height (50));
+		GUILayout.EndHorizontal ();
+
+		GUILayout.EndArea ();
+
+	}
+}
