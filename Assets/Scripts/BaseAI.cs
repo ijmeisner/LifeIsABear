612aae6a25bc891eb571b3de2ad08efd9aa6fcef
agiles231 <agiles231@gmail.com>
2014-10-04 13:18:41 -0400

    BaseAI interface
    The interface that all other AI packages will inherit from.

---
+++ Assets/Scripts/BaseAI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface BaseAI
+{
+	void smell();         // Smell for predators/prey
+	void hear();          // Hear sounds
+	void lineOfSight();   // Detect things in sight
+	void pathSelect();    // Select path to move along
+	void attack();        // Attack something
+	void lookForOthers(); // Look for other living beings
+	void flee();          // Run under various situations
+	void hide();
+}
